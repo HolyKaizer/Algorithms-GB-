d304b0559e5f14e615868002445ce0cf0afff14b
Anton Kaizer <kaizer_1997@icloud.com>
2019-12-03 00:22:34 +0300

    Initial Commit

---
+++ Algorithms(GB)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithms(GB)
+//
+//  Created by Антон Кайзер on 03.12.2019.
+//  Copyright © 2019 Антон Кайзер. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
