52960d157825fca6e9f6e67b7503c910f0f0cf9b
Anton Kaizer <kaizer_1997@icloud.com>
2019-12-17 18:31:54 +0300

    Complete homework-5

--- Algorithms(GB)/main.c
+++ Algorithms(GB)/main.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define T char
 //
@@ -93,6 +94,9 @@ int push(Stack *st, T val) {
 }
 
 T pop(Stack *st) {
+    if (st->size == 0) {
+        return -1;
+    }
     Node * temp = st->head;
     
     st->head = st->head->next;
@@ -103,8 +107,151 @@ T pop(Stack *st) {
     
     return result;
 }
+/*------------------------------------Домащняя работа 5----------------------------------------*/
+//Задание номер 1.
+//    Реализовать перевод из десятичной в двоичную систему счисления с использованием стека.
+void from_two_to_ten(int n, Stack *st) {
+    while (n != 0) {
+        if (n % 2)
+            push(st, '1');
+        else
+            push(st, '0');
+        n /= 2;
+    }
+}
+//Задание номер 2.
+//    Написать программу, которая определяет,
+//    является ли введенная скобочная последовательность правильной.
+//    Примеры правильных скобочных выражений: (), ([])(), {}(),
+//    ([{}]),
+//    неправильных — )(, ())({), (, ])}), ([(]) для скобок [, (, {.
+//    Например: (2+(2*2)) или [2/{5*(4+7)}].
+int correct_bracket_seq(char * str) {
+    int i;
+    Stack s;
+    s.size = 0;
+    s.head = NULL;
+    for (i = 0; i < strlen(str); i++) {
+        if ((str[i] == '(') || (str[i] == '[') || (str[i] == '{')){ //Если скобка открывающая
+            push(&s, str[i]);
+        } else if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}')){ //Если скобка закрывающая
+            T test = pop(&s);
+            if (test == -1)
+                return 0;
+            else {                  //Если совпадают виды скобок - все верно, иначе плохая последовательность
+                if ((test == '(') && (str[i] == ')'))
+                    continue;
+                else if ((test == '[') && (str[i] == ']'))
+                    continue;
+                else if ((test == '{') && (str[i] == '}'))
+                    continue;
+                else
+                    return 0;
+            }
+        }
+    }
+    
+    if (s.size) //Если стек остался пустым - последовательность закрылась, иначе остались открывающие скобки
+        return 0;
+    else
+        return 1;
+}
+
+//Задание номер 3.
+//    Создать функцию, копирующую односвязный список
+//     (то есть создающую в памяти копию односвязного списка без удаления первого списка)
+
+
+
+//Задание номер 4.
+//    Реализовать очередь на двусвязном списке.
+typedef struct Node2{
+    T data;
+    struct Node2 *next;
+    struct Node2 *prev;
+} Node2;
+
+typedef struct {
+    Node2 * head;
+    Node2 * tail;
+    int size;
+} Queue;
+
+int add(Queue *qu, T value) {
+    Node2 * temp = (Node2*) malloc(sizeof(Node2));
+    if (temp == NULL) return 0;
+    
+    temp->data = value;
+    temp->next = qu->head;
+    temp->prev = NULL;
+    
+    
+    if (qu->head != NULL) {
+        qu->head->prev = temp;
+    } else {
+        qu->tail = temp;
+    }
+    
+    qu->head = temp;
+    qu->size++;
+    return 1;
+}
+
+T delete(Queue *qu) {
+    if (!qu->size) {
+        return -1;
+    }
+    
+    Node2 * temp = qu->tail;
+    
+    T result = temp->data;
+    
+    qu->tail = qu->tail->prev;
+    qu->size--;
+    free(temp);
+    
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
+    Stack s;
+    s.size = 0;
+    s.head = NULL;
+
+    /*-----------------Домащняя работа 5-------------*/
+    
+    /*-----Задание номер 1-----*/
+    from_two_to_ten(60, &s);
+    while (s.size > 0) {
+        printf("%c", pop(&s));
+    }
+    printf("\n");
+    pop(&s);
+
+    /*-----Задание номер 2-----*/
+    
+    if(correct_bracket_seq("([{asfkjalsf}])"))
+        printf("ВЕРНАЯ ПОСЛЕДОВАТЕЛЬНОСТЬ\n");
+    else
+        printf("НЕ ВЕРНАЯ ПОСЛЕДОВАТЕЛЬНОСТЬ\n");
+    
+    /*---Задание номер 4---*/
+    Queue q;
+    q.size = 0;
+    q.head = NULL;
+    q.tail = NULL;
+    
+    add(&q, '1');
+    add(&q, '2');
+    add(&q, '3');
+    add(&q, '4');
+    add(&q, '5');
+    
+    while (q.size > 0) {
+        printf("%c", delete(&q));
+    }
+    printf("\n");
+    delete(&q);
     
     
 }
